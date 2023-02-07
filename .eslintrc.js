42fcffd9adc1685c7a55125e071a6bc710a475dd
blackparadise0407 <blackparadise0407@gmail.com>
2023-02-07 21:12:22 +0700

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		tsconfigRootDir: __dirname,
+	},
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+	},
+	plugins: ['@typescript-eslint/eslint-plugin', 'import', 'svelte3'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'plugin:import/recommended',
+	],
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		'no-console': 'warn',
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
+		'import/no-unresolved': 'off',
+		'import/order': [
+			'error',
+			{
+				pathGroups: [
+					{
+						pattern: '@/**',
+						group: 'internal',
+					},
+				],
+				groups: ['builtin', 'external', 'internal'],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				'newlines-between': 'always',
+			},
+		],
+	},
+};
