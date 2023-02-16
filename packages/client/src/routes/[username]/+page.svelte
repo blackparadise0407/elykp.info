457058ee6d651a7118a057edc96c7af3dabd50a5
blackparadise0407 <blackparadise0407@gmail.com>
2023-02-16 17:56:26 +0700

    fix: css

--- packages/client/src/routes/[username]/+page.svelte
+++ packages/client/src/routes/[username]/+page.svelte
@@ -19,8 +19,8 @@
 				class="bg-orange-500 h-36 bg-center bg-no-repeat bg-cover"
 				style="background-image: url(https://images.unsplash.com/photo-1505598872760-6090aa9ed603?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60);"
 			/>
-			<div class="p-5 md:p-10 -mt-16 space-y-1">
-				<Avatar cls="!w-[100px] border-4 border-white " {user} />
+			<div class="p-5 md:p-8 space-y-1">
+				<Avatar cls="!w-[100px] -mt-20 border-4 border-white " {user} />
 				<h1 class="font-semibold text-base md:text-lg">{user.first_name} {user.last_name}</h1>
 				<p class="text-sm text-gray-500">Los Angeles, United States</p>
 				<p>@{user.username}</p>
