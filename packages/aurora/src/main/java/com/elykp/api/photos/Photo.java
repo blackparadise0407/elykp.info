03f8de4dab2a69be303de6fbfc1519ddd692914c
blackparadise0407 <blackparadise0407@gmail.com>
2023-03-01 00:06:07 +0700

    feat: implement photo entity

--- packages/aurora/src/main/java/com/elykp/api/photos/Photo.java
+++ packages/aurora/src/main/java/com/elykp/api/photos/Photo.java
@@ -1,11 +1,9 @@
 package com.elykp.api.photos;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Data
 @Entity
@@ -18,6 +16,23 @@ public class Photo {
     @Column(length = 8)
     private String id;
 
-    @Column(name = "creator_id")
+    private String title;
+
+    @Column(name = "creator_id", length = 36)
     private String creatorId;
+
+    @Column(name = "like_count", columnDefinition = "integer default 0")
+    private Integer likeCount;
+
+    @Column(name = "nsfw", columnDefinition = "boolean default false")
+    private Boolean nsfw;
+
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        if (this.createdAt == null)
+            createdAt = Instant.now().getEpochSecond();
+    }
 }
